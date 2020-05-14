4183762eb2c3fd1517edb943ec246695051bb6ff
edy <cordoncool1@gmail.com>
2020-05-14 13:36:46 +0200

    //////

--- Practica5/src/Dominio/Persona.java
+++ Practica5/src/Dominio/Persona.java
@@ -1,18 +1,16 @@
 package Dominio;
 
 public class Persona {
-<<<<<<< HEAD
+
 	
 	private String loggin;
-=======
+
 
 	private String password;
 	private String nombre;
->>>>>>> branch 'master' of https://github.com/edy789/Practica5-Buena.git
+
 	private int edad;
-<<<<<<< HEAD
 
-=======
 
->>>>>>> branch 'master' of https://github.com/edy789/Practica5-Buena.git
+
 }
