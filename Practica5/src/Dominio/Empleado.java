07dc75e41a4a6d9d872a6bc08dc605c4873cc8fc
medeinjuan@gmail.com <medeinjuan@gmail.com>
2020-05-14 14:39:08 +0200

    se genera toString en empleado

--- Practica5/src/Dominio/Empleado.java
+++ Practica5/src/Dominio/Empleado.java
@@ -26,6 +26,11 @@ public class Empleado {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
+	@Override
+	public String toString() {
+		return "Empleado [loggin=" + loggin + ", password=" + password + "]";
+	}
 	
 	
 }
