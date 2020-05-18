6e7d08fddc2b60db8174817feefc6df0ef0bdb22
medeinjuan@gmail.com <medeinjuan@gmail.com>
2020-05-18 14:12:06 +0200

    Se actualiza tanto empleado como empleadoDao para que tenga todos los metodos

--- Practica5/src/Dominio/Empleado.java
+++ Practica5/src/Dominio/Empleado.java
@@ -1,12 +1,18 @@
 package Dominio;
 
+import java.util.ArrayList;
+
+import Persistencia.EmpleadoDao;
+
 public class Empleado {
 	private String loggin;
 	private String password;
+	private EmpleadoDao empleadoDao;
 	
 	public Empleado(String loggin, String password) {
 		this.loggin = loggin;
 		this.password = password;
+		empleadoDao= new EmpleadoDao();
 	}
 
 	public String getLoggin() {
@@ -30,5 +36,31 @@ public class Empleado {
 		return "Empleado [loggin=" + loggin + ", password=" + password + "]";
 	}
 	
+	public void insertar() throws ClassNotFoundException {
+		empleadoDao.insertar(this);
+	}
+
+	public ArrayList<Empleado> leerTodos() throws ClassNotFoundException {
+		return empleadoDao.leerTodos();
+
+	}
+	public Empleado leerPersona(String loggin) throws ClassNotFoundException {
+		return empleadoDao.leer(loggin);
+
+	}
+
+	public void actualizar(String loggin) throws ClassNotFoundException {
+		empleadoDao.actualizar(this, loggin);
+
+	}
+
+	public void eliminar() throws ClassNotFoundException {
+		empleadoDao.eliminar(this);
+	}
+
+	public void eliminarTodo() throws ClassNotFoundException {
+		empleadoDao.eliminarTodo();
+
+	}
 	
 }
