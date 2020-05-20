a2b01901dfae3fe2f391a55797fd47ac4efb2d1c
medeinjuan@gmail.com <medeinjuan@gmail.com>
2020-05-20 13:32:46 +0200

    Se instancia todos los metodos de camiones y de turismo

--- Practica5/src/Dominio/VehiculoCamiones.java
+++ Practica5/src/Dominio/VehiculoCamiones.java
@@ -1,7 +1,6 @@
 package Dominio;
 
 import java.util.ArrayList;
-import Persistencia.ExtrasVehiTurismosDao;
 import Persistencia.VehiculoCamionesDao;
 
 public class VehiculoCamiones extends Vehiculo{
@@ -24,39 +23,35 @@ public class VehiculoCamiones extends Vehiculo{
 	public void setCapacarga(double capacarga) {
 		this.capacarga = capacarga;
 	}
-
 	@Override
 	public void insertar() throws ClassNotFoundException {
-		// TODO Auto-generated method stub
-	}
-
-	@Override
-	public ArrayList<Vehiculo> leerTodos() throws ClassNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Vehiculo leerPersona(String matricula) throws ClassNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void actualizar(String matricula) throws ClassNotFoundException {
-		// TODO Auto-generated method stub	
-	}
-
-	@Override
-	public void eliminar() throws ClassNotFoundException {
-		// TODO Auto-generated method stub	
-	}
-
-	@Override
-	public void eliminarTodo() throws ClassNotFoundException {
-		// TODO Auto-generated method stub
+		vehiculoCamionesDao.insertar(this);
 		
 	}
+	@Override
+	public ArrayList<Vehiculo> leerTodos() throws ClassNotFoundException {
+		return vehiculoCamionesDao.leerTodos();
+	}
+	@Override
+	public Vehiculo leerPersona(String matricula) throws ClassNotFoundException {
+		return vehiculoCamionesDao.leer(matricula);
+	}
+	@Override
+	public void actualizar(String matricula) throws ClassNotFoundException {
+		vehiculoCamionesDao.actualizar(this, matricula);
+	}
+	@Override
+	public void eliminar() throws ClassNotFoundException {
+		vehiculoCamionesDao.eliminar(this);
+	}
+	@Override
+	public void eliminarTodo() throws ClassNotFoundException {
+		vehiculoCamionesDao.eliminarTodo();
+		
+	}
+	
+	
+	
 
 	
 }
